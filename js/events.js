d5b5e4c91885d04c7583823a91385e31549955c7
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-30 23:41:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,22 @@ $(document).ready(function(){
 // call functions here
 
 });
+function getIt() {
+  
+}
+function frameIt () {
+  
+}
+function submitIt() {
+  $("form").on("submit", function () {
+    if ($( "input:first").val() === "correct") {
+      alert('your form is going to be submitted now');
+      return
+    }
+    alert("you entered the wrong value");
+    return
+  })
+}
+function pressIt () {
+  
+}
