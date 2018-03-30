303a51d88a80ad26cbca5d7ffd9050309611c5d9
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-30 23:53:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,12 +16,7 @@ function frameIt () {
 //Define a function submitIt that does not accept a parameter. The function should bind a submit event to the form that alerts "Your form is going to be submitted now."
 function submitIt() {
   $("form").on("submit", function () {
-    if ($( "input:first").val() === "correct") {
       alert('Your form is going to be submitted now');
-      return
-    }
-    alert("you entered the wrong value");
-    return
   })
 }
 //Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
