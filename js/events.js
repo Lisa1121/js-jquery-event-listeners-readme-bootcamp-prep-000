2279a5fe02879cada31430e5ef0a244b5014702c
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-30 23:48:05 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,6 +23,7 @@ function submitIt() {
     return
   })
 }
+//Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
 function pressIt () {
   
 }
